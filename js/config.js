ba8ba54c70fc5b3408a5ee97c0c98ff966978f63
AlexisGuil <4w67jggfcj@privaterelay.appleid.com>
2026-06-23 14:40:11 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // ← COLLEZ ICI VOTRE URL APPS SCRIPT (après déploiement)
-  API_URL: "https://script.google.com/macros/s/AKfycbxyiy3N0khkTscEFoxlXmLkHoclvmLS8wjbEV7GowUt-1wjHnus3L1QdK87a1xZuoIb/exec",
+ API_URL: "https://script.google.com/macros/s/AKfycbxyiy3N0khkTscEFox1XmLkHoc1vmLS8wjbEV7GowUt-1wjHnus3L1QdK87a1xZuoIb/exec",
 
   // ← VOTRE CODE PIN (doit être identique à celui dans Apps Script)
   PIN: "1234",
